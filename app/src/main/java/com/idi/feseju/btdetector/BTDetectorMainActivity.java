a25cdd8f49251925d29f31434a0e05986d85bcfc
IDIDesarrollo <ididesarrollo@gmail.com>
2016-01-15 11:13:22 +0100

    First commit BTDetector. Android 4.4 SDK 19

---
+++ app/src/main/java/com/idi/feseju/btdetector/BTDetectorMainActivity.java
@@ -0,0 +1,13 @@
+package com.idi.feseju.btdetector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BTDetectorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_btdetector_main);
+    }
+}
